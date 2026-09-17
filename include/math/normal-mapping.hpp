37b6311f7364aa285a3d0201636eeb5ef2f1f1c7
Nikita Fediuchin <cfnptr@gmail.com>
2026-09-17 16:40:54 +0300

    Add Octahedron Normal Encoding

--- include/math/normal-mapping.hpp
+++ include/math/normal-mapping.hpp
@@ -33,6 +33,59 @@ static float3 unpackNormal(float3 packedNormal) noexcept { return fma(packedNorm
 static f32x4 packNormal(f32x4 packedNormal) noexcept { return fma(packedNormal, f32x4(0.5f), f32x4(0.5f)); }
 static f32x4 unpackNormal(f32x4 packedNormal) noexcept { return fma(packedNormal, f32x4(2.0f), f32x4(-1.0f)); }
 
+static uint32 packNormal3x10(float3 normal) noexcept
+{
+	auto packed = uint3(fma(packNormal(normal), float3(1023.0f), float3(0.5f)));
+	return packed.x | (packed.y << 10u) | (packed.z << 20u);
+}
+static float3 unpackNormal3x10(uint32 packed) noexcept
+{
+	auto normal = float3(uint3(packed, packed >> 10u, packed >> 20u) & 1023u);
+	return normalize(unpackNormal(normal * float(1.0 / 1023.0)));
+}
+
+//**********************************************************************************************************************
+// Signed Octahedron Normal Encoding (R10G10B10A2)
+// https://johnwhite3d.blogspot.com/2017/10/signed-octahedron-normal-encoding.html
+
+static float2 projectNormToOctXY(float3 normal) noexcept
+{
+	return (float2)normal / dot3(abs((f32x4)normal), f32x4::one);
+}
+
+static float2 encodeNormalOct2(float3 normal) noexcept // [result in -1.0 - 1.0 range]
+{
+	auto projNorm = projectNormToOctXY(normal);
+	auto normSign = float2(normal.x >= 0.0f ? 1.0f : -1.0f, normal.y >= 0.0f ? 1.0f : -1.0f);
+	return normal.z >= 0.0f ? projNorm : (1.0f - abs(float2(projNorm.y, projNorm.x))) * normSign;
+}
+static float3 decodeNormalOct2(float2 encoded) noexcept
+{
+	auto normal = float3(encoded, 1.0f - (std::abs(encoded.x) + std::abs(encoded.y)));
+	auto t = saturate(-normal.z);
+	normal.x += normal.x >= 0.0f ? -t : t;
+	normal.y += normal.y >= 0.0f ? -t : t;
+	return normalize(normal);
+}
+
+static float3 encodeNormalOct3(float3 normal) noexcept // [result in 0.0 - 1.0 range]
+{
+	auto projNorm = projectNormToOctXY(normal);
+	auto y = std::fma(projNorm.y, 0.5f, 0.5f);
+	auto x = std::fma(projNorm.x, 0.5f, y);
+	y = std::fma(projNorm.x, -0.5f, y);
+	auto z = saturate(normal.z * FLT_MAX);
+	return float3(x, y, z);
+}
+static float3 decodeNormalOct3(float3 encoded) noexcept
+{
+	auto normal = float3(encoded.x - encoded.y, 
+		encoded.x + encoded.y - 1.0f, std::fma(encoded.z, 2.0f, -1.0f));
+	normal.z *= 1.0f - (std::abs(normal.x) + std::abs(normal.y));
+	return normalize(normal);
+}
+
+//**********************************************************************************************************************
 static f32x4x4 computeTBN(const f32x4x4& model, f32x4 normal, f32x4 tangent) noexcept
 {
 	assert(normal == normalize3(normal));
@@ -42,6 +95,14 @@ static f32x4x4 computeTBN(const f32x4x4& model, f32x4 normal, f32x4 tangent) noe
 	t = normalize3(t - dot3(t, n) * n);
 	auto b = cross3(n, t);
 	return f32x4x4(t, b, n);
+}
+static f32x4x4 computeTBN(f32x4 normal) noexcept
+{
+	auto up = std::abs(normal.getZ()) < 0.999f ? 
+		f32x4(0.0f, 0.0f, 1.0f) : f32x4(1.0f, 0.0f, 0.0f);
+	auto tangent = normalize3(cross3(up, normal));
+	auto bitangent = cross3(normal, tangent);
+	return f32x4x4(tangent, bitangent, normal);
 }
 
 static f32x4x4 approximateTBN(f32x4 normal) noexcept
